4b2724836695f7752155479db43589d33cd3ee54
mrtgdev <mrtg.dev@outlook.com>
2022-12-20 20:37:56 +0100

    Ejercicio 01 > ¡Automatizando envolver regalos de navidad!

---
+++ code_day_01.js
@@ -0,0 +1,24 @@
+/**
+ * Puntos conseguidos: 110
+ * 2517 ops/s
+ * Complejidad cognitiva: 1
+ */
+
+const gifts = ['cat', 'game', 'socks'];
+const wrapped = wrapping(gifts);
+
+console.log(wrapped);
+
+// > Resultado
+function wrapping(gifts) {
+  const result = [];
+  gifts.forEach( (gift, index) => {
+    let paper = '**';
+    for ( let i=0; i<gift.length; i++ ) {
+      paper += '*';
+    }
+    result.push( paper + '\n*' + gift + '*\n' + paper );
+  });
+
+  return result;
+}
