766b870feda94e7f65e057e3999d750541b70606
Mario Ortega Palacios <mortegap.ext@ayesa.com>
2022-12-22 11:58:43 +0100

    Reto 16 > Arreglando las cartas

--- code_day_16.js
+++ code_day_16.js
@@ -1,13 +1,20 @@
+/**
+ * ⭐ Puntos conseguidos: 200
+ * 🕑 1875 ops/s
+ * 🎯 Complejidad cognitiva: 1
+ */
 
 fixLetter(` hello,  how are you??     do you know if santa claus exists?  i really hope he does!  bye  `);
-// Resultado --> Hello, how are you? Do you know if Santa Claus exists? I really hope he does! Bye.
 
-console.log(fixLetter(` hello,  how are you??     do you know if santa claus exists?  i really hope he does!  bye  `))
+// Resultado --> Hello, how are you? Do you know if Santa Claus exists? I really hope he does! Bye.
 
 function fixLetter( letter ) {
     return letter
-        .replace('??', '?')
-        .replace('santa claus', 'Santa Claus')
-        .replace('', '')
-        .replace(',', ', ');
+        .replace(/(^\s+)|(\s+$)/g, '')
+        .replace(/([.,!])(.*)/g, '$1 $2')
+        .replace(/([.,?!\s])(?=\1)/g, '')
+        .replace(/\s+([.,?!])/g, '$1')
+        .replace(/santa claus/gi, 'Santa Claus')
+        .replace(/\b([.?!] \w)|(^\w)/g, match => match.toUpperCase())
+        .replace(/^.*\w$/, match => `${match}.`);
 }
